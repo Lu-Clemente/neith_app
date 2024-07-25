c62c0c7f2cb572aaa280f785e5403183289a27e3
Luciene Clemente <luclemente.dev@gmail.com>
2024-07-25 18:51:09 -0300

    feat: complete login screen

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:neith/views/auth/email_confirmation_view.dart';
 import 'package:neith/views/auth/forget_password.dart';
+import 'package:neith/widgets/buttons/neith_icon_button.dart';
+import 'package:neith/widgets/buttons/neith_text_button.dart';
+import 'package:neith/widgets/inputs/neith_text_field.dart';
 
 import 'register_view.dart';
 import '../home_view.dart';
@@ -61,59 +65,115 @@ class LoginViewState extends State<LoginView> {
     );
   }
 
+  _goToRegister() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const RegisterView()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Form(
           key: _formKey,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/logo.png'),
+                      height: 104.94,
+                      width: 93.38,
+                    ),
+                    const SizedBox(height: 46.6),
+                    Text(
+                      'Login to your account',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(height: 20),
+                    NeithTextField(
+                      controller: _emailController,
+                      labelText: 'Email',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 40),
+                    NeithTextField(
+                      controller: _passwordController,
+                      labelText: 'Password',
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: _goToForgetPassword,
+                        child: const Text(
+                          'Forgot your Password?',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    NeithTextButton(
+                      onPressed: _login,
+                      label: 'Sign In',
+                      textColor: Colors.white,
+                      backgroundColor: const Color(0xFF1F1B59),
+                    ),
+                    const SizedBox(height: 10),
+                    NeithIconButton(
+                      onPressed: () {},
+                      iconPath: 'assets/google_icon.png',
+                      label: 'Sign In with Google',
+                      textColor: Colors.black,
+                      backgroundColor: const Color(0xFFF2F2F2),
+                    ),
+                    const SizedBox(height: 10),
+                    NeithIconButton(
+                      onPressed: () {},
+                      iconPath: 'assets/meta_icon.png',
+                      label: 'Sign In with Meta',
+                      textColor: Colors.white,
+                      backgroundColor: const Color(0xFF007EF7),
+                    ),
+                  ],
+                ),
               ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
+              RichText(
+                text: TextSpan(
+                  text: "Don't have an account? ",
+                  style: const TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: 'Sign Up',
+                      style: const TextStyle(
+                        color: Color(0xFF1F1B59),
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = _goToRegister,
+                    ),
+                  ],
+                ),
               ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                child: const Text('Login'),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const RegisterView()),
-                  );
-                },
-                child: const Text("Don't have an account? Register"),
-              ),
-              TextButton(
-                onPressed: _goToForgetPassword,
-                child: const Text('Forgot Password?'),
-              ),
             ],
           ),
         ),
