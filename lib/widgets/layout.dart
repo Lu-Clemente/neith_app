5a1c52d44ac1311f8e2b1c5c093c58e62690d6ea
Luciene Clemente <luclemente.dev@gmail.com>
2024-07-27 19:20:48 -0300

    feat: Layout wrapper component to views

---
+++ lib/widgets/layout.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:neith/widgets/app_bar/neith_app_bar.dart';
+
+class Layout extends StatelessWidget {
+  final Widget body;
+  final EdgeInsets padding;
+  final NeithAppBar? appBar;
+
+  const Layout({
+    Key? key,
+    required this.body,
+    this.padding = const EdgeInsets.all(20.0),
+    this.appBar,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: appBar != null
+          ? Column(
+              children: [
+                appBar!,
+                Padding(
+                  padding: padding,
+                  child: body,
+                ),
+              ],
+            )
+          : Padding(
+              padding: padding,
+              child: body,
+            ),
+    );
+  }
+}
