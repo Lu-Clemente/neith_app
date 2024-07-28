78017767caa16b618aca1b860e0557cbce17666b
Luciene Clemente <luclemente.dev@gmail.com>
2024-07-27 23:02:08 -0300

    feat: new Forget Password view

--- lib/views/auth/forget_password.dart
+++ lib/views/auth/forget_password.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:neith/widgets/app_bar/neith_app_bar.dart';
+import 'package:neith/widgets/buttons/neith_text_button.dart';
+import 'package:neith/widgets/inputs/neith_text_field.dart';
+import 'package:neith/widgets/layout.dart';
 
 class ForgotPasswordView extends StatefulWidget {
   const ForgotPasswordView({super.key});
@@ -35,33 +39,51 @@ class ForgotPasswordViewState extends State<ForgotPasswordView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Forgot Password'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _resetPassword,
-                child: const Text('Reset Password'),
-              ),
-            ],
-          ),
+    return Layout(
+      appBar: const NeithAppBar(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Forgot your password?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  'Enter your email below to receive instructions to reset your password.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                NeithTextField(
+                  controller: _emailController,
+                  labelText: 'Email',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 40),
+                NeithTextButton(
+                  onPressed: _resetPassword,
+                  label: 'Recover my password',
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
