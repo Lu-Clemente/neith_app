f70e12074cdd80c5cfb763907c62e7f1f5202ea7
Luciene Clemente <luclemente.dev@gmail.com>
2024-07-28 00:08:29 -0300

    feat: new Reset Password view

---
+++ lib/views/auth/reset_password_view.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:neith/views/home_view.dart';
+import 'package:neith/widgets/buttons/neith_text_button.dart';
+import 'package:neith/widgets/inputs/neith_text_field.dart';
+import 'package:neith/widgets/app_bar/neith_app_bar.dart';
+import 'package:neith/widgets/layout.dart';
+
+class ResetPasswordView extends StatefulWidget {
+  const ResetPasswordView({super.key});
+
+  @override
+  ResetPasswordViewState createState() => ResetPasswordViewState();
+}
+
+class ResetPasswordViewState extends State<ResetPasswordView> {
+  final _formKey = GlobalKey<FormState>();
+  final _newPasswordController = TextEditingController();
+  final _newPasswordConfirmationController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _resetPassword() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      try {
+        final user = _auth.currentUser;
+        if (user != null) {
+          await user.updatePassword(_newPasswordController.text.trim());
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Password reset successful')),
+          );
+          await Future.delayed(const Duration(seconds: 2));
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeView(),
+            ),
+          );
+        }
+      } on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password reset failed: ${e.message}')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      appBar: const NeithAppBar(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Reset your password',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  'Create a new password below to secure your account.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                NeithTextField(
+                  controller: _newPasswordController,
+                  labelText: 'New password',
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your new password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                NeithTextField(
+                  controller: _newPasswordConfirmationController,
+                  labelText: 'Confirm new password',
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please confirm your new password';
+                    }
+                    if (value != _newPasswordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 40),
+                NeithTextButton(
+                  onPressed: _resetPassword,
+                  label: 'Create new password',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
