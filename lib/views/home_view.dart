dbb4559545efe60662d17a9788e2508c7e4b42b9
Luciene Clemente <luclemente.dev@gmail.com>
2024-07-27 23:39:45 -0300

    feat: app bar data display

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,59 +1,44 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import 'auth/login_view.dart';
+import 'package:neith/utils/time.dart';
+import 'package:neith/widgets/app_bar/neith_app_bar.dart';
+import 'package:neith/widgets/layout.dart';
 
 class HomeView extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   HomeView({super.key});
 
-  Future<void> _signOut(context) async {
-    try {
-      await _auth.signOut();
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginView()),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Sign out failed: $e')),
-      );
-    }
-  }
-
   String _getUser() {
     final user = _auth.currentUser;
-    return user?.email ?? 'User';
+    return user?.displayName ?? 'dear user';
+  }
+
+  Future<void> _registerUserName() async {
+    final user = _auth.currentUser;
+    if (user?.displayName == null) {
+      await user?.updateDisplayName('Lu Clemente');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () => _signOut(context),
-            ),
+    return Layout(
+      appBar: NeithAppBar(
+        actions: const [
+          NeithAppBarAction.profile,
+          NeithAppBarAction.notifications,
+        ],
+        showBackButton: false,
+        title: 'Welcome, ${_getUser()}!',
+        subtitle: 'Today, ${getTodayDate()}',
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Placeholder(),
           ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('assets/logo.png'),
-                height: 100,
-              ),
-              const SizedBox(height: 40),
-              Text('Welcome ${_getUser()}'),
-            ],
-          ),
         ),
       ),
     );
