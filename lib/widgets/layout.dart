004b1c46f9e0df9a5c7d31a84619c82c288b30be
Luciene Clemente <luclemente.dev@gmail.com>
2024-08-02 09:33:55 -0300

    fix: Layout fix for ios

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:neith/widgets/app_bar/neith_app_bar.dart';
 
@@ -15,24 +17,52 @@ class Layout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: appBar != null
-            ? Column(
-                children: [
-                  appBar!,
-                  Padding(
-                    padding: padding,
-                    child: body,
-                  ),
-                ],
-              )
-            : Padding(
-                padding: padding,
-                child: body,
-              ),
-      ),
+    return Platform.isAndroid
+        ? SafeArea(
+            child: BaseLayout(
+              appBar: appBar,
+              padding: padding,
+              body: body,
+            ),
+          )
+        : BaseLayout(
+            appBar: appBar,
+            padding: padding,
+            body: body,
+          );
+  }
+}
+
+class BaseLayout extends StatelessWidget {
+  const BaseLayout({
+    super.key,
+    required this.appBar,
+    required this.padding,
+    required this.body,
+  });
+
+  final NeithAppBar? appBar;
+  final EdgeInsets padding;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: appBar != null
+          ? Column(
+              children: [
+                appBar!,
+                Padding(
+                  padding: padding,
+                  child: body,
+                ),
+              ],
+            )
+          : Padding(
+              padding: padding,
+              child: body,
+            ),
     );
   }
 }
